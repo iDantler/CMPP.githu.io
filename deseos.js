33b262875d6d95399f2643d387ba2cdf0f42a33d
iDantler <pirlxgamer@gmail.com>
2023-04-26 22:46:50 -0400

    Añadí otro menu de alerta para volver a introducir deseo mañana y un gato que baila xd

--- deseos.js
+++ deseos.js
@@ -3,47 +3,48 @@ $(document).ready(function() {
   var submitButton = document.querySelector(".submit-button");
   var inputBox = document.querySelector(".input-box");
   
-  // Obtener el deseo del día actual del objeto localStorage
-  var today = new Date().toLocaleDateString();
-  var todayWish = localStorage.getItem(today);
-  
-  if (todayWish) {
-    // Si ya hay un deseo para hoy, mostrarlo en el contenedor
-    var newParagraph = document.createElement("p");
-    newParagraph.innerText = todayWish;
-    textContainer.appendChild(newParagraph);
-  }
-  
   submitButton.addEventListener("click", function() {
     var text = inputBox.value;
     if (text !== "") {
-      if (todayWish) {
-        // Si ya hay un deseo para hoy, lanzar un error
-        alert("Ya has ingresado un deseo para hoy.");
-      } else {
-        // Si no hay un deseo para hoy, agregarlo y almacenarlo en el objeto localStorage
-        var newParagraph = document.createElement("p");
-        newParagraph.innerText = text;
-        textContainer.appendChild(newParagraph);
-        inputBox.value = "";
-        
-        localStorage.setItem(today, text);
-        
-        // Enviar el deseo a la hoja de cálculo
-        $.ajax({
-          url:"https://api.apispreadsheets.com/data/sF94Hh0T9oktrxei/",
-          type:"post",
-          data:{
-            "Deseos:": text
-          },
-          success: function(){
-            alert("Tu deseo está siendo procesado ♥")
-          },
-          error: function(){
-            alert("Aiudaa, me caí... Awanta tantito.")
-          }
+      // Obtener el deseo del local storage y comprobar si ya hay uno guardado para hoy
+      var storedWish = localStorage.getItem("deseo");
+      var today = new Date();
+      var storedDate = new Date(storedWish);
+      if (storedWish && storedDate.getDate() === today.getDate() && storedDate.getMonth() === today.getMonth() && storedDate.getFullYear() === today.getFullYear()) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Ay no :(',
+          text: 'Tu deseo fue muy potente we...',
+          footer: '<a href="./gato/gato-pelao-bailando.mp4">Ven mañana y coloca otro deseo.</a>'
         });
+        return;
       }
+      // Guardar el deseo en el local storage
+      localStorage.setItem("deseo", today.toString());
+
+      // Agregar el deseo al contenedor de textos
+      var newParagraph = document.createElement("p");
+      newParagraph.innerText = text;
+      textContainer.appendChild(newParagraph);
+      inputBox.value = "";
+      
+      // Enviar el deseo a la hoja de cálculo usando AJAX
+      $.ajax({
+        url:"https://api.apispreadsheets.com/data/sF94Hh0T9oktrxei/",
+        type:"post",
+        data:{
+          "Deseos:": text
+        },
+        success: function(){
+          alert("Tu deseo está siendo procesado ♥");
+          setTimeout(function() {
+            location.reload();
+          }, 2000);
+        },
+        error: function(){
+          console.log("There was an error :(")
+        }
+      });
     }
   });
 });
